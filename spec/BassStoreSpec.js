5e64d99c88a7e8b71740db7ec0733d52448c9e35
Anthony Matarazzo <createvibe@gmail.com>
2017-08-17 16:59:29 -0700

    mixins use merge; fix bass session test spec

--- spec/BassStoreSpec.js
+++ spec/BassStoreSpec.js
@@ -2,6 +2,23 @@ const path = require('path');
 
 const Kernel = require('@conga/framework/lib/kernel/TestKernel');
 
+const rootPath = path.join(__dirname, '..');
+const modulePath = path.join(rootPath, 'node_modules');
+const appPath = path.join(rootPath, 'spec', 'data', 'projects', 'sample');
+
+const Module = require('module');
+const originalLoad = Module._load;
+
+Module._load = function (request, parent) {
+
+    let idx = request.indexOf('bass-nedb');
+    if (idx !== -1) {
+        request = path.join(modulePath, request.substr(idx));
+    }
+
+    return originalLoad.call(this, request, parent);
+};
+
 describe("Bass Store", () => {
 
     let kernel;
@@ -9,16 +26,16 @@ describe("Bass Store", () => {
     beforeAll((done) => {
 
         kernel = new Kernel(
-            path.join(__dirname, '..', 'spec', 'data', 'projects', 'sample'),
+            appPath,
             'app',
             'bass_store',
             {}
         );
 
         kernel.addBundlePaths({
-            'demo-bundle': path.join(__dirname, '..', 'spec', 'data', 'projects', 'sample', 'src', 'demo-bundle'),
-            '@conga/framework-session': path.join(__dirname, '..'),
-            '@conga/framework-bass': path.join(__dirname, '..', 'node_modules', '@conga', 'framework-bass')
+            'demo-bundle': path.join(appPath, 'src', 'demo-bundle'),
+            '@conga/framework-session': rootPath,
+            '@conga/framework-bass': path.join(modulePath, '@conga', 'framework-bass')
         });
 
         kernel.boot(() => {
